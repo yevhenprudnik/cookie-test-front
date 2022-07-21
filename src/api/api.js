2b324230b8fdb9c63b4c65e8828f1ba761ba5aa9
yevhenprudnik <yevhenprudnik@gmail.com>
2022-07-21 21:22:15 +0300

    env2

--- src/api/api.js
+++ src/api/api.js
@@ -7,8 +7,8 @@ export const api = axios.create({
 const DevUrl = "http://localhost:3001/api"
 const ProductionUrl = "https://cookie-test-server-1903.herokuapp.com/api"
 
-export const baseUrl = process.env.NODE_ENV === "production" ? ProductionUrl : DevUrl
-
+export const baseUrl = process.env.NODE_ENV === 'development' ? DevUrl : ProductionUrl
+console.log(process.env.NODE_ENV)
 api.interceptors.response.use((config) => {
   return config
 }, async (error) =>{
